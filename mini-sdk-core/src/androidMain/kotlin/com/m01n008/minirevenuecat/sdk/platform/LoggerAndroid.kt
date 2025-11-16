bb857649686e6fe6f83f9b4a2cbe945e60b8f008
Moin Khan <iammoin008@gmail.com>
2025-11-17 00:48:11 +0500

    updated LoggerAndroid.kt in commonMain to not use android.util.log to verify MiniPurchasesTest.kt

--- mini-sdk-core/src/androidMain/kotlin/com/m01n008/minirevenuecat/sdk/platform/LoggerAndroid.kt
+++ mini-sdk-core/src/androidMain/kotlin/com/m01n008/minirevenuecat/sdk/platform/LoggerAndroid.kt
@@ -1,15 +1,15 @@
 package com.m01n008.minirevenuecat.sdk.platform
 
-import android.util.Log
+
 
 actual object Logger {
     private const val TAG = "MiniRC"
 
     actual fun d(message: String) {
-      Log.d(TAG, message)
+        println("DEBUG: [$TAG] $message")
     }
 
     actual fun e(message: String) {
-        Log.e(TAG, message)
+        println("ERROR: [$TAG] $message")
     }
 }
