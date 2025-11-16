bcd18c1bc0798ed1f285f9917bbc3e77739b3687
Moin Khan <iammoin008@gmail.com>
2025-11-16 21:39:25 +0500

    -fixed timestamp elapsed logic (it was taking entry time near to 0 or 0ms which would make the condition of get always true and time to live cache logic failing)
    -added tests and verified my TTLCacheTest.kt for put and get is working perfectly

--- mini-sdk-core/src/commonMain/kotlin/com/m01n008/minirevenuecat/sdk/caches/TTLCache.kt
+++ mini-sdk-core/src/commonMain/kotlin/com/m01n008/minirevenuecat/sdk/caches/TTLCache.kt
@@ -14,20 +14,20 @@ class TTlCache<K, V>(
 
     private data class CacheEntry<V> (
         val value: V,
-        val timestamp: Long
+        val timestamp: TimeSource.Monotonic.ValueTimeMark
         )
 
     suspend fun put(key: K, value: V) {
         mutex.withLock {
-            cache[key] = CacheEntry(value, timeSource.markNow().elapsedNow().inWholeMilliseconds)
+            cache[key] = CacheEntry(value, timeSource.markNow())
         }
     }
 
-    suspend fun get(key: K) {
+    suspend fun get(key: K): V? {
         return mutex.withLock {
-            val now = timeSource.markNow().elapsedNow().inWholeMilliseconds
+
             val entry = cache[key]
-            if (entry != null && (now - entry.timestamp) <= ttl.inWholeMilliseconds) {
+            if (entry != null &&  entry.timestamp.elapsedNow() <= ttl) {
                 entry.value
             } else {
                 cache.remove(key)
--- mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/TTLCacheTest.kt
+++ mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/TTLCacheTest.kt
@@ -1,2 +1,18 @@
 package com.m01n008.minirevenuecat.sdk
 
+import com.m01n008.minirevenuecat.sdk.caches.TTlCache
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
+
+class TTLCacheTest {
+
+    @Test
+    fun testPutAndGet() = runBlocking {
+        val cache = TTlCache<String, String>(ttl = 2.seconds)
+        cache.put("key1", "value1")
+        val value = cache.get("key1").toString()
+        assertEquals("value1", value)
+    }
+}
