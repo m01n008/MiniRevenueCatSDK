3b75d0eda8c1be8320ff1d9d1842b367374a2804
Moin Khan <iammoin008@gmail.com>
2025-11-16 21:50:05 +0500

    -Created ExponentialBackoffTest.kt and added and verified retrySucceeds and failure cases

--- mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/ExponentialBackoffTest.kt
+++ mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/ExponentialBackoffTest.kt
@@ -1,2 +1,30 @@
 package com.m01n008.minirevenuecat.sdk
 
+import com.m01n008.minirevenuecat.sdk.backoff.ExponentialBackoff
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import kotlin.time.Duration.Companion.milliseconds
+
+class ExponentialBackoffTest {
+
+    @Test
+    fun testRetrySucceeds() = runBlocking {
+        val backoff  = ExponentialBackoff(initialDelay = 10.milliseconds, maxDelay = 50.milliseconds, maxRetries = 3)
+        var attempt = 0
+        val result = backoff.retry {
+            attempt++
+            attempt >= 2
+        }
+        assertTrue(result)
+    }
+
+    @Test
+    fun testRetryFails() = runBlocking {
+        val backoff = ExponentialBackoff(initialDelay = 10.milliseconds, maxDelay = 50.milliseconds, maxRetries = 2)
+        val result = backoff.retry {
+            false
+        }
+        assertTrue(!result)
+    }
+}
