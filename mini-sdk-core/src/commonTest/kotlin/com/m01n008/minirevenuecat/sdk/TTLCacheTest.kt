9a02258ab65a867eaa767f7b06dcfae1b711f765
Moin Khan <iammoin008@gmail.com>
2025-11-16 21:43:41 +0500

    -added tests in TTLCacheTest.kt for expiry is working perfectly

--- mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/TTLCacheTest.kt
+++ mini-sdk-core/src/commonTest/kotlin/com/m01n008/minirevenuecat/sdk/TTLCacheTest.kt
@@ -4,6 +4,7 @@ import com.m01n008.minirevenuecat.sdk.caches.TTlCache
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.time.Duration.Companion.seconds
 
 class TTLCacheTest {
@@ -12,7 +13,16 @@ class TTLCacheTest {
     fun testPutAndGet() = runBlocking {
         val cache = TTlCache<String, String>(ttl = 2.seconds)
         cache.put("key1", "value1")
-        val value = cache.get("key1").toString()
+        val value = cache.get("key1")
         assertEquals("value1", value)
+    }
+
+    @Test
+    fun testExpiry() = runBlocking {
+        val cache = TTlCache<String, String>(ttl = 1.seconds)
+        cache.put("key2", "value2")
+        kotlinx.coroutines.delay(1500)
+        val value = cache.get("key2")
+        assertNull(value)
     }
 }
